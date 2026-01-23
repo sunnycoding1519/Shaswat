75156340dd3f032c5135856b4ffa35ef399eb953
sunnycoding1519 <sunnyyadav805133@gmail.com>
2026-01-23 22:45:08 +0530

    TIME UPDATE AJMER CENTER

--- src/pages/Centers.jsx
+++ src/pages/Centers.jsx
@@ -23,7 +23,7 @@ const centers = [
     address:
       "Krishnpura,vp.O-Bithur,Via-Rajgarh,Ajmer,Rajasthan-305205",
     phone: "+91 8619452121 (Laxman Singh)",
-    timing: "Mon – Sat : 8:00 AM – 4:00 PM",
+    timing: "ONLY SUNDAY : 9:00 AM – 5:00 PM ",
     image: "/Images/centers/centers 2.jpeg",
     map:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3573.1136728657007!2d74.6180165!3d26.41980805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396be0ca268ef1bb%3A0xd5ce70f16947a2e!2sKishanpura%2C%20Chandra%20Vardai%20Nagar%2C%20Ajmer%2C%20Rajasthan%20305003!5e0!3m2!1sen!2sin!4v1768834469547!5m2!1sen!2sin\" width=\"600\" height=\"450\" style=\"border: 0;\" allowfullscreen=\"\" loading=\"lazy\" referrerpolicy=\"no-referrer-when-downgrade\"></iframe>",
