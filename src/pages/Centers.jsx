e0226e1fe3cc7c8a67d8c8870eefbf1963a56e07
sunnycoding1519 <sunnyyadav805133@gmail.com>
2026-01-31 17:51:19 +0530

    img update

--- src/pages/Centers.jsx
+++ src/pages/Centers.jsx
@@ -36,10 +36,10 @@ const centers = [
     name: "Shashwat Chikitsa Kendra – Kanpur",
     address:
       "M.I.G-8,Barra-6,Kanpur Nagar(UP)– 208027",
-    phone: "Dr.Anoop Kushwaha (+91 73550 95660)",
+    phone: "Dr.Anoop Kushwaha (+91 9559906407)",
     timing: "Mon – Sat : 8:00 AM – 4:00 PM",
-    /*image: "/Images/centers/center UP.jpeg",
-    map:
+    image: "/Images/centers/Kanpur.jpg",
+    /*map:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5975.778133642752!2d80.94156777770996!3d26.913884999999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399957c2c3d0fd13%3A0x871ab3b0fedf760!2sJankipuram%2C%20Lucknow!5e1!3m2!1sen!2sin!4v1767985897468!5m2!1sen!2sin",
     scripturl:
       "https://script.google.com/macros/s/AKfycbxaI8p4I87OKFh9eEsr9lPkqAQTlIqBNTlwdnL3n0B1gx_492vTKswJdvR2i1s288O5/exec"*/
