847d6e9cf24bb2e385d2900387765ec61383aebb
sunnycoding1519 <sunnyyadav805133@gmail.com>
2026-01-31 16:46:18 +0530

    New center Update\

--- src/pages/Centers.jsx
+++ src/pages/Centers.jsx
@@ -29,6 +29,20 @@ const centers = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3573.1136728657007!2d74.6180165!3d26.41980805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396be0ca268ef1bb%3A0xd5ce70f16947a2e!2sKishanpura%2C%20Chandra%20Vardai%20Nagar%2C%20Ajmer%2C%20Rajasthan%20305003!5e0!3m2!1sen!2sin!4v1768834469547!5m2!1sen!2sin\" width=\"600\" height=\"450\" style=\"border: 0;\" allowfullscreen=\"\" loading=\"lazy\" referrerpolicy=\"no-referrer-when-downgrade\"></iframe>",
     scripturl:
       "https://script.google.com/macros/s/AKfycbzlf7w0rtA48x_dnZD_zirl4SGQ94j6bxotxpXKbWrQZYx6c6h70iPm7G9lbrOiy2KPdA/exec"
+  },
+  {
+    id: 3,
+    city: "Kanpur",
+    name: "Shashwat Chikitsa Kendra – Kanpur",
+    address:
+      "M.I.G-8,Barra-6,Kanpur Nagar(UP)– 208027",
+    phone: "Dr.Anoop Kushwaha (+91 73550 95660)",
+    timing: "Mon – Sat : 8:00 AM – 4:00 PM",
+    /*image: "/Images/centers/center UP.jpeg",
+    map:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5975.778133642752!2d80.94156777770996!3d26.913884999999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399957c2c3d0fd13%3A0x871ab3b0fedf760!2sJankipuram%2C%20Lucknow!5e1!3m2!1sen!2sin!4v1767985897468!5m2!1sen!2sin",
+    scripturl:
+      "https://script.google.com/macros/s/AKfycbxaI8p4I87OKFh9eEsr9lPkqAQTlIqBNTlwdnL3n0B1gx_492vTKswJdvR2i1s288O5/exec"*/
   }
 ];
 
