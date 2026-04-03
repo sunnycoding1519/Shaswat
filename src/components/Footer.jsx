8993e68a57151585d5f7e934bb28125a691bbc5c
sunnycoding1519 <sunnyyadav805133@gmail.com>
2026-04-03 11:28:49 +0530

    number update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -41,7 +41,11 @@ export default function Footer() {
           <div className="footer-contact">
             <p>
               📞 Helpline:&nbsp;
-              <a href="tel:+917355095660">+91 73550 95660/+91 9250588545</a>
+              <a href="tel:+917355095660">+91 73550 95660</a>
+            </p>
+            <p>
+              📞 Helpline:&nbsp;
+              <a href="tel:+919250588545">+91 92505 88545</a>
             </p>
 
             <p>
